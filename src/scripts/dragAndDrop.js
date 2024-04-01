af231354bfdda9e02f7c6f228127a855eed6b1ac
amarukawanobiru <k4tsu0502@gmail.com>
2024-04-01 18:46:54 +0900

    🚀 スライダー式のパラメータのコードをタッチデバイスでも実行されるように修正

--- src/scripts/dragAndDrop.js
+++ src/scripts/dragAndDrop.js
@@ -7,82 +7,78 @@ if ($indicator) {
 
   let shiftX;
   // let shiftY;
-  function mouseMove(event) {
+
+  function updateBackdropFilter() {
+    const denominator =
+      $indicator.clientWidth - $indicatorPointer.clientWidth - 6;
+
+    const ratio = Math.trunc(
+      (($indicatorPointer.getBoundingClientRect().left -
+        $indicator.getBoundingClientRect().left -
+        2) /
+        denominator) *
+        100
+    );
+
+    $mainVisualMask.style.backdropFilter = `blur(${Math.trunc(ratio / 5)}px)`;
+    $mainVisualMask.style.WebkitBackdropFilter = `blur(${Math.trunc(
+      ratio / 5
+    )}px)`;
+    $status.textContent = `${ratio}%`;
+  }
+
+  function adjustIndicatorPosition(
+    grabElementLeft,
+    pointerPositionX,
+    deviceType
+  ) {
+    $indicatorPointer.style.left = `${
+      pointerPositionX - $indicator.getBoundingClientRect().left - shiftX
+    }px`;
+
+    if (grabElementLeft - 2 < $indicator.getBoundingClientRect().left) {
+      $indicatorPointer.style.left = "2px";
+      if (deviceType === "touch") {
+        document.removeEventListener("touchmove", handleTouchMove);
+      } else {
+        document.removeEventListener("mousemove", handleMouseMove);
+      }
+    }
+
+    if (
+      grabElementLeft + $indicatorPointer.clientWidth + 4 >
+      $indicator.getBoundingClientRect().left + $indicator.clientWidth
+    ) {
+      $indicatorPointer.style.left = `calc(100% - 4px - ${$indicatorPointer.clientWidth}px)`;
+      if (deviceType === "touch") {
+        document.removeEventListener("touchmove", handleTouchMove);
+      } else {
+        document.removeEventListener("mousemove", handleMouseMove);
+      }
+    }
+
+    updateBackdropFilter();
+  }
+
+  function handleMouseMove(event) {
     const grabElementLeft = $indicatorPointer.getBoundingClientRect().left;
 
     const pointerPositionX = event.pageX;
     // let pointerPositionY = event.changedTouches[0].pageY ? event.changedTouches[0].pageY : event.pageY;
 
-    $indicatorPointer.style.left = `${
-      pointerPositionX - $indicator.getBoundingClientRect().left - shiftX
-    }px`;
-
-    const denominator =
-      $indicator.clientWidth - $indicatorPointer.clientWidth - 6;
-
-    if (grabElementLeft - 2 < $indicator.getBoundingClientRect().left) {
-      $indicatorPointer.style.left = "2px";
-      document.removeEventListener("mousemove", mouseMove);
-    }
-
-    if (
-      grabElementLeft + $indicatorPointer.clientWidth + 4 >
-      $indicator.getBoundingClientRect().left + $indicator.clientWidth
-    ) {
-      $indicatorPointer.style.left = `calc(100% - 4px - ${$indicatorPointer.clientWidth}px)`;
-      document.removeEventListener("mousemove", mouseMove);
-    }
-
-    const ratio = Math.trunc(
-      (($indicatorPointer.getBoundingClientRect().left -
-        $indicator.getBoundingClientRect().left -
-        2) /
-        denominator) *
-        100
-    );
-
-    $mainVisualMask.style.backdropFilter = `blur(${Math.trunc(ratio / 5)}px)`;
-    $status.textContent = `${ratio}%`;
+    adjustIndicatorPosition(grabElementLeft, pointerPositionX, "mouse");
   }
 
-  function touchMove(event) {
+  function handleTouchMove(event) {
     const grabElementLeft = $indicatorPointer.getBoundingClientRect().left;
 
     let pointerPositionX = event.changedTouches[0].pageX;
     // let pointerPositionY = event.changedTouches[0].pageY ? event.changedTouches[0].pageY : event.pageY;
 
-    $indicatorPointer.style.left = `${
-      pointerPositionX - $indicator.getBoundingClientRect().left - shiftX
-    }px`;
-
-    const denominator =
-      $indicator.clientWidth - $indicatorPointer.clientWidth - 6;
-
-    if (grabElementLeft - 2 < $indicator.getBoundingClientRect().left) {
-      $indicatorPointer.style.left = "2px";
-      document.removeEventListener("touchmove", touchMove);
-    }
-
-    if (
-      grabElementLeft + $indicatorPointer.clientWidth + 4 >
-      $indicator.getBoundingClientRect().left + $indicator.clientWidth
-    ) {
-      $indicatorPointer.style.left = `calc(100% - 4px - ${$indicatorPointer.clientWidth}px)`;
-      document.removeEventListener("touchmove", touchMove);
-    }
-
-    const ratio = Math.trunc(
-      (($indicatorPointer.getBoundingClientRect().left -
-        $indicator.getBoundingClientRect().left -
-        2) /
-        denominator) *
-        100
-    );
-
-    $mainVisualMask.style.backdropFilter = `blur(${Math.trunc(ratio / 5)}px)`;
-    $status.textContent = `${ratio}%`;
+    adjustIndicatorPosition(grabElementLeft, pointerPositionX, "touch");
   }
 
+  // ポインティングデバイスがマウスだった場合の処理
   $indicatorPointer.addEventListener("mousedown", (event) => {
     event.preventDefault();
     $indicatorPointer.style.cursor = "grabbing";
@@ -90,14 +86,15 @@ if ($indicator) {
     shiftX = event.clientX - $indicatorPointer.getBoundingClientRect().left;
     // shiftY = event.clientY - $indicatorPointer.getBoundingClientRect().top;
 
-    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mousemove", handleMouseMove);
 
     document.addEventListener("mouseup", () => {
       $indicatorPointer.style.cursor = "grab";
-      document.removeEventListener("mousemove", mouseMove);
+      document.removeEventListener("mousemove", handleMouseMove);
     });
   });
 
+  // ポインティングデバイスのがタッチだった場合の処理
   $indicatorPointer.addEventListener("touchstart", (event) => {
     event.preventDefault();
 
@@ -105,10 +102,10 @@ if ($indicator) {
       event.changedTouches[0].clientX -
       $indicatorPointer.getBoundingClientRect().left;
 
-    document.addEventListener("touchmove", touchMove);
+    document.addEventListener("touchmove", handleTouchMove);
 
     document.addEventListener("touchend", () => {
-      document.removeEventListener("touchmove", touchMove);
+      document.removeEventListener("touchmove", handleTouchMove);
     });
   });
 }
